904dc93398310b3cbe38e821e19d9d0618f987e5
TooFiveFive <e.moynihan00@gmail.com>
2017-12-06 23:46:56 +0000

    not done..???

--- routes/middleware.js
+++ routes/middleware.js
@@ -12,6 +12,14 @@ function get_data(airport) {
 
     pyshell[pyshell.length - 1].send(airport);
 
+    pyshell[pyshell.length - 1].end(function (err) {
+        if (err) {
+            console.log(err);
+            pyshell[pyshell.length - 1] = null;
+        }
+        console.log('finished');
+    });
+
 }
 
 module.exports = {'get_data': get_data, 'pyshell': pyshell};
